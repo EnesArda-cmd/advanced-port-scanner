f62d68ab66c978706eb8ac57ff2e5b1501f973b2
EnesArda-cmd <bodurenesarda@gmail.com>
2026-04-27 22:11:30 +0300

    port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,31 @@
 import socket
 
+print("="*30)
+print(" Simple Port Scanner ")
+print("="*30)
+
 target = input("Enter target IP: ")
+
 ports = [21, 22, 23, 25, 53, 80, 443]
+open_ports = []
 
 print(f"\nScanning {target}...\n")
 
 for port in ports:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
-    
+
     result = s.connect_ex((target, port))
-    
+
     if result == 0:
-        print(f"Port {port} is OPEN")
-    
+        print(f"[OPEN] Port {port}")
+        open_ports.append(port)
+
     s.close()
+
+print("\nScan completed.")
+
+if open_ports:
+    print("Open ports:", open_ports)
+else:
+    print("No open ports found.")
